e2cbb8310f170e68dce5d25a4e44976d20131660
danielchowyc <daniellchow@gmail.com>
2020-09-04 20:02:31 +0100

    added messages homepage

--- express-mini-message-board/routes/index.js
+++ express-mini-message-board/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hello world! Does anybody use this website?",
+    user: "Daniel",
+    added: new Date()
+  },
+  {
+    text: "Hello there this is my message! ",
+    user: "James",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages: messages});
 });
 
 module.exports = router;
